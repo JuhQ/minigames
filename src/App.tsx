dbb49a9de9874f26c6a8c6792884d3e3bf1b52f4
Juha Tauriainen <juha@bin.fi>
2025-01-08 20:37:30 +0200

    feat: wrap Router in Suspense for improved loading experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
+import React, { Suspense } from "react";
 import { Route, HashRouter as Router, Routes } from "react-router-dom";
-
-import React from "react";
 
 const Home = React.lazy(() => import("./pages/Home"));
 const HangmanPage = React.lazy(() => import("./pages/Hangman"));
@@ -8,15 +7,17 @@ const MemoryPage = React.lazy(() => import("./pages/Memory"));
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen container m-0 mx-auto dark:text-white">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/hangman" element={<HangmanPage />} />
-          <Route path="/memory" element={<MemoryPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Router>
+        <div className="min-h-screen container m-0 mx-auto dark:text-white">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/hangman" element={<HangmanPage />} />
+            <Route path="/memory" element={<MemoryPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </Suspense>
   );
 }
 
